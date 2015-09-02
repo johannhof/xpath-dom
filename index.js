498dae374bf9aa8db9f4c0d3730a0740c94b4a09
Johann <git@johann-hofmann.com>
2015-09-02 10:35:48 +0200

    Implement getXPath

--- index.js
+++ index.js
@@ -19,7 +19,31 @@ exports.findAll = function(expression, relativeTo) {
   return nodes;
 };
 
-exports.getXPath = function(node) {
-  //while(node !== document){
-  //}
+exports.getXPath = function(node, relativeTo) {
+  relativeTo = relativeTo || document.body;
+  var lookBreaker = 0;
+  var path = '';
+  while(node !== relativeTo){
+    if(lookBreaker++ > 1000){
+      throw new Error('Got to maximum traversal level without reaching the root node.');
+    }
+    var position = 1;
+    var siblings = node.parentNode.children;
+    for (var i = 0; i < siblings.length; i++) {
+      if(siblings[i] === node){
+        break;
+      }
+      if(siblings[i].nodeName === node.nodeName){
+        position++;
+      }
+    }
+    var pos = position > 1 ? "[" + position + "]" : '';
+    path = node.nodeName + pos + '/' + path;
+    node = node.parentNode;
+  }
+
+  // remove the trailing slash
+  path = path.slice(0, -1);
+
+  return path.toLowerCase();
 };
