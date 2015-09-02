33b48fd46cc3bd9709cb0c51f8721dec08003583
Johann <git@johann-hofmann.com>
2015-09-02 10:36:13 +0200

    Update tests on gh-pages

--- build/xpath-dom.js
+++ build/xpath-dom.js
@@ -58,6 +58,10 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports) {
 
 	exports.find = function(expression, relativeTo) {
+	  return exports.findAll(expression, relativeTo)[0];
+	};
+
+	exports.findAll = function(expression, relativeTo) {
 	  var snapshot = document.evaluate(
 	    expression,
 	    relativeTo || document.body,
@@ -74,9 +78,33 @@ return /******/ (function(modules) { // webpackBootstrap
 	  return nodes;
 	};
 
-	exports.getXPath = function(node) {
-	  //while(node !== document){
-	  //}
+	exports.getXPath = function(node, relativeTo) {
+	  relativeTo = relativeTo || document.body;
+	  var lookBreaker = 0;
+	  var path = '';
+	  while(node !== relativeTo){
+	    if(lookBreaker++ > 1000){
+	      throw new Error('Got to maximum traversal level without reaching the root node.');
+	    }
+	    var position = 1;
+	    var siblings = node.parentNode.children;
+	    for (var i = 0; i < siblings.length; i++) {
+	      if(siblings[i] === node){
+	        break;
+	      }
+	      if(siblings[i].nodeName === node.nodeName){
+	        position++;
+	      }
+	    }
+	    var pos = position > 1 ? "[" + position + "]" : '';
+	    path = node.nodeName + pos + '/' + path;
+	    node = node.parentNode;
+	  }
+
+	  // remove the trailing slash
+	  path = path.slice(0, -1);
+
+	  return path.toLowerCase();
 	};
 
 
